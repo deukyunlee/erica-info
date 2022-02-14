0a39e80cdb6043f029605a88edebfaab49d3e3b6
deukyunlee <dukedev1004@gmail.com>
2022-02-14 23:07:52 +0900

    FIX: Bug fix

--- routes/crawlPages.js
+++ routes/crawlPages.js
@@ -18,40 +18,37 @@ const crawlUrl = function (url) {
           List[i].date = List[i].date.split(".").join("-");
         });
         const data = List.filter((n) => n.title); // 같이 바꾸기
-        // const jsonData = JSON.stringify(data);
-        // jsonData2 = "{" + '"' + "symbols" + '"' + ":" + jsonData + "}";
-        // fs.writeFileSync("./symbol.json", jsonData2);
-        // console.log("JSON created");
+
         resolve(data);
       }
     });
   });
 };
 
-const crawlUrl2 = function (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, (error, response, html) => {
-      if (!error && response.statusCode === 200) {
-        const $ = cheerio.load(html);
-        const $symbolResults = $(".bbs_con").find("tbody").find("tr");
-        $symbolResults.each(function (i, result) {
-          List[i] = {
-            type: $(this).find("td:nth-of-type(1)").text(),
-            // title: $(this).find("td.left").find("a").text(),
-            // url: url + $(this).find("td.left").find("a").attr("href"),
-            // date: $(this).find("td:nth-of-type(3)").text(),
-          };
-          List[i].date = List[i].date.split(".").join("-");
-        });
-        const data = List.filter((n) => n.title); // 같이 바꾸기
-        // const jsonData = JSON.stringify(data);
-        // jsonData2 = "{" + '"' + "symbols" + '"' + ":" + jsonData + "}";
-        // fs.writeFileSync("./symbol.json", jsonData2);
-        // console.log("JSON created");
-        resolve(data);
-      }
-    });
-  });
-};
+// const crawlUrl2 = function (url) {
+//   return new Promise(function (resolve, reject) {
+//     request(url, (error, response, html) => {
+//       if (!error && response.statusCode === 200) {
+//         const $ = cheerio.load(html);
+//         const $symbolResults = $(".bbs_con").find("tbody").find("tr");
+//         $symbolResults.each(function (i, result) {
+//           List[i] = {
+//             type: $(this).find("td:nth-of-type(1)").text(),
+//             // title: $(this).find("td.left").find("a").text(),
+//             // url: url + $(this).find("td.left").find("a").attr("href"),
+//             // date: $(this).find("td:nth-of-type(3)").text(),
+//           };
+//           List[i].date = List[i].date.split(".").join("-");
+//         });
+//         const data = List.filter((n) => n.title); // 같이 바꾸기
+//         // const jsonData = JSON.stringify(data);
+//         // jsonData2 = "{" + '"' + "symbols" + '"' + ":" + jsonData + "}";
+//         // fs.writeFileSync("./symbol.json", jsonData2);
+//         // console.log("JSON created");
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
 
 module.exports = { crawlUrl, crawlUrl2 };
